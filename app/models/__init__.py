696c866eff17d7a3dd925fd8914540a2764c9716
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-08-04 23:22:04 +0000

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,23 +1,34 @@
-# app/models/__init__.py
-from app.models.notification import Notification
-from app.models.user import User
-from app.models.employee import Employee
+ # app/models/__init__.py
+from app.models.notification import notifications
+from app.models.user import users
+from app.models.employee import departments, job_titles, employees
 from app.models.attendance import Attendance
 from app.models.analytics import Report, Metric
-from app.models.audit import Audit
-from app.models.payroll import Payroll
-from app.models.performance import Performance
-from app.models.recruitment import Recruitment
+from app.models.audit import audit_logs
+from app.models.payroll import salaries, payslips
+from app.models.performance import performance_reviews, goals, kpis
+from app.models.recruitment import job_posts, applications, interviews
 __all__ = [
-    "Notification",
-    "User",
-    "Employee",
+    "notifications",
+    "users",
+    "Employees",
     "Attendance",
-    "Analytics",
-    "Audit",
+    "Report",
+    "Metric",
+    "audit_logs",
     "Payroll",
     "Performance",
-    "Recruitment"
-]
+    "Recruitment",
+    "departments",
+    "job_titles",
+    "payslips",
+    "salaries",
+    "performance_reviews",
+    "goals",
+    "kpis",
+    "job_posts",
+    "applications",
+    "interviews"  
 
+]
 
