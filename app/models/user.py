fdc2650dcbe83ec4bfbefc1d9ec0b6c66cd03b48
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-08-04 04:57:52 +0000

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,8 @@
- class User(Base):
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from app.database import Base
+
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -8,3 +12,8 @@
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+
+
+
+
