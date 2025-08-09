1cbd33431fbac82a1c8a4130e0938f52540beef1
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-08-09 05:37:38 +0000

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,22 @@
-# Empty file to mark app/api as a package
+ from .employee import *
+from .admin import *
+from .aanalytics import *
+from .attendance  import *
+from .audit  import *
+from .auth import *
+from .finance import *
+from .hr import *
+from .mobile  import *
+from .notifications  import *
+from .payroll import *
+from .payslip import *
+from .performance import *
+from .recruitment  import *
+from .user  import *
+
+
+
+
+
+
 
